6b30ddd9952770c8b8b46edaca8740098e4bc8d4
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-05 21:18:22 +0100

    Added responsive notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   Spacer,
   Text,
+  useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
@@ -11,38 +12,31 @@ import { RiDeleteBin2Fill } from "react-icons/ri";
 
 const Note = () => {
   return (
-    <div>
-      <Box
-        maxW="sm"
-        minH="sm"
-        borderWidth="1px"
-        overflow="hidden"
-        borderRadius="lg"
-      >
-        {/* Note Header */}
-        <VStack>
-          <Text p="3" fontWeight="semibold">
-            Title
-          </Text>
-        </VStack>
-        {/* Note Content */}
-        <VStack>
-          <Text w="100%" pl="5" minH="280px">
-            This is content
-          </Text>
-        </VStack>
-        {/* Note Footer */}
-        <HStack p="3">
-          <Text>This is date</Text>
-          <Spacer />
-          <IconButton
-            isRound="true"
-            bg="transparent"
-            icon={<RiDeleteBin2Fill />}
-          />
-        </HStack>
-      </Box>
-    </div>
+    <Box
+      bg={useColorModeValue("gray.100", "gray.900")}
+      minW="230px"
+      borderWidth="1px"
+      overflow="hidden"
+      borderRadius="lg"
+      mx="5"
+    >
+      {/* Note Content */}
+      <VStack>
+        <Text w="100%" p="5" minH="200px">
+          This is content
+        </Text>
+      </VStack>
+      {/* Note Footer */}
+      <HStack p="3">
+        <Text>This is date</Text>
+        <Spacer />
+        <IconButton
+          isRound="true"
+          bg="transparent"
+          icon={<RiDeleteBin2Fill />}
+        />
+      </HStack>
+    </Box>
   );
 };
 
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,11 +1,17 @@
+import { Flex, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import Note from "./Note";
 
 const NotesList = () => {
   return (
-    <div>
-      <Note />
-    </div>
+    <Flex justify="center">
+      <SimpleGrid columns={[1, 2, 3]} pt="5" spacingY="5" maxW="1000px">
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </SimpleGrid>
+    </Flex>
   );
 };
 
