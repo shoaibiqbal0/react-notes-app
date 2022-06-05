aa5005246c39dae64f1ab3f18ea9345ff7bff826
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-05 21:34:28 +0100

    Deleted index.css

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -6,11 +6,11 @@ const NotesList = () => {
   const [notes, setNotes] = useState([
     { content: "This is content for note 1", date: "20/01/2022" },
     { content: "This is content for note 2", date: "20/02/2022" },
-    { content: "This is content for note 3", date: "22/03/2023" },
+    { content: "This is content for note 2", date: "20/02/2022" },
   ]);
   return (
     <Flex justify="center">
-      <SimpleGrid columns={[1, 2, 3]} pt="5" spacing="5" maxW="1000px">
+      <SimpleGrid columns={[2, 2, 3]} pt="5" spacing="5" maxW="1000px">
         {notes.map((note, index) => {
           return <Note key={index} date={note.date} content={note.content} />;
         })}
